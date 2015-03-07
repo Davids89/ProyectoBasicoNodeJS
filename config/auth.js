3d1d542c47f1937d301b33886a4f398f87e48787
David <daviluqui@gmail.com>
2015-03-07 10:41:10 +0100

    Arreglado bug

--- config/auth.js
+++ config/auth.js
@@ -4,22 +4,22 @@
 
 module.exports = {
 	'facebookAuth' : {
-		'clientID' : '1553433908246425', //esta es la APP ID
-		'clientSecret' : 'aa7f77bb950723d5e56627e06715b526', //La clave secreta que genera Facebook
+		'clientID' : '', //esta es la APP ID
+		'clientSecret' : '', //La clave secreta que genera Facebook
 		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
 		/*la callback es necesaria para asegurarnos de que identificamos con FB y hacemos un redireccionamiento
 		a los usuarios de vuelta a nuestra app después de haber aprobado su acceso a la misma*/
 	},
 
 	'twitterAuth' : {
-		'consumerKey' : 'hbkwinTR5EpC1KKSujUqf1CCv',
-		'consumerSecret' : 'Akln7OKjsTVq1dyUSsJ0Y3dxanVPg5BPQI75QTxp0QQoFBAn3E',
+		'consumerKey' : '',
+		'consumerSecret' : '',
 		'callbackURL' : 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
-		'clientID' : 'a',
-		'clientSecret' : 'a',
+		'clientID' : '',
+		'clientSecret' : '',
 		'callbackURL' : 'http://localhost:8080/auth/google/callback'
 	}
 };
